3859ce8190874b3faeab37ef1db326fad80ed6d8
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-12-06 00:51:29 -0800

    add more examples

--- src/script.js
+++ src/script.js
@@ -20,14 +20,37 @@ function positionHoverBoxes() {
 
 	var b1 = document.getElementById("b1");
 	var c1 = document.getElementById("c1");
+	var b2 = document.getElementById("b2");
+	var c2 = document.getElementById("c2");
+	var b3 = document.getElementById("b3");
+	var c3 = document.getElementById("c3");
+	var b4 = document.getElementById("b4");
+	var c4 = document.getElementById("c4");
 
-	b1.style.left = articleRect.left - b1.offsetWidth + 25 + "px";
-	b1.style.top = articleRect.top + window.scrollY + 1000 + "px";
+	setupLeftBox(articleRect, b1, c1, 500);
+	setupRightBox(articleRect, b2, c2, 600);
+	setupRightBox(articleRect, b3, c3, 800);
+	setupLeftBox(articleRect, b4, c4, 1300);
+}
 
-	c1.style.left = b1.offsetLeft + "px";
-	c1.style.top = b1.offsetTop + "px";
-	c1.style.width = b1.offsetWidth + "px";
-	c1.style.height = b1.offsetHeight + "px";
+function setupLeftBox(a, b, c, y) {
+	b.style.left = a.left - b.offsetWidth + 25 + "px";
+	b.style.top = a.top + window.scrollY + y + "px";
+	c.style.left = b.offsetLeft + "px";
+	c.style.top = b.offsetTop + "px";
+	c.style.width = b.offsetWidth + "px";
+	c.style.height = b.offsetHeight + "px";
+	b.style.textAlign = "right";
+}
+
+function setupRightBox(a, b, c, y) {
+	b.style.left = a.right - 25 + "px";
+	b.style.top = a.top + window.scrollY + y + "px";
+	c.style.left = b.offsetLeft + "px";
+	c.style.top = b.offsetTop + "px";
+	c.style.width = b.offsetWidth + "px";
+	c.style.height = b.offsetHeight + "px";
+	b.style.textAlign = "left";
 }
 
 positionHoverBoxes();
